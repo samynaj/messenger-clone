33a406de67d6f7755d6d99e86d30b5752adfa142
samynaj <samuelnnj@gmail.com>
2022-03-08 18:17:55 +0100

    feeds component

---
+++ src/components/feeds/Feeds.jsx
@@ -0,0 +1,14 @@
+import Share from "../share/Share"
+import "./feeds.css"
+
+function Feeds() {
+  return (
+    <div className="feed">
+        <div className="feedWrapper">
+             <Share />
+        </div>
+    </div>
+  )
+}
+
+export default Feeds
