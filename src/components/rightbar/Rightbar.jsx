cd6a36874c49e30883f4af55e34ca167ac4ef543
samynaj <samuelnnj@gmail.com>
2022-03-12 22:46:19 +0100

    rightbar component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,21 +1,62 @@
+import Ad from "../ad/Ad"
 import Online from "../online/Online"
 import "./rightbar.css"
 
-function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+function Rightbar({profile}) {
+
+  const HomeRightBar = () => {
+    return (
+      <>
+         <div className="birthdayContainer">
           <img src="assets/gift.png" alt="" className="birthdayImg" />
           <span className="birthdayText">
             <b>Palmer Jones</b> and <b>3 other friends</b> have their birthday today.
           </span>
         </div>
-        <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <Ad />
         <h4 className="rightbarTitle">Online Friends</h4>
         <ul className="rightbarFriendList">
           <Online />
         </ul>
+      </>
+    )
+  }
+
+  const ProfileRightBar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoItemKey">City:</span>
+            <span className="rightbarInfoItemValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoItemKey">Home Town:</span>
+            <span className="rightbarInfoItemValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoItemKey">Relationship:</span>
+            <span className="rightbarInfoItemValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Linda Cole</span>
+          </div>
+        </div>
+        <Ad />
+      </>
+    )
+  }
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+       {
+         profile? <ProfileRightBar /> : <HomeRightBar />
+       }
       </div>
     </div>
   )
