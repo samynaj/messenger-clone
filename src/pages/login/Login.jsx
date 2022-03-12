47c83365cf67b691a60763c924edde57301974ff
samynaj <samuelnnj@gmail.com>
2022-03-12 22:47:09 +0100

    login

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login'>
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Snow Chat</h3>
+                <span className="loginDesc">Connect with friends and the world around you on Snow Chat</span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
