0cc1c809763cc5040bbea8571af7758085b363a8
samynaj <samuelnnj@gmail.com>
2022-03-12 16:55:06 +0100

    friend component

---
+++ src/components/friend/Friend.jsx
@@ -0,0 +1,12 @@
+import './friend.css'
+
+function Friend() {
+  return (
+    <li className="friend">
+        <img src="/assets/person/2.jpeg" alt="person" className="friendImg" />
+        <span className="friendName">Jane Doe</span>
+    </li>
+  )
+}
+
+export default Friend
